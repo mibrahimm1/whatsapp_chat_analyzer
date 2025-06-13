aa0b28c34795a8255bacf21a24c742fdff702cbb
mibrahimm1 <149292702+mibrahimm1@users.noreply.github.com>
2025-06-14 01:42:24 +0500

    Update helper.py

--- helper.py
+++ helper.py
@@ -74,7 +74,7 @@ def create_wordcloud(selected_user, df):
 
 def most_common_words(selected_user, df):
     # Load Stop Words Mask
-    f = open('files\stopwords.txt', 'r')
+    f = open('files/stopwords.txt', 'r')
     stop_words = f.read()
 
     if selected_user != 'Overall':
